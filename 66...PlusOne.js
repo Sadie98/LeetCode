63ca2b83e86a60f3a21596bade7e8a05e11d663a
Uliana Avdeeva <avdeeva_121212@mail.ru>
2020-03-27 19:24:47 +0300

    Create 66...PlusOne.js

---
+++ 66...PlusOne.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    for(let i = digits.length - 1; i >= 0; i--){
+        digits[i] += 1;
+        if(digits[i] > 9){
+            digits[i] = 0;
+            if(i === 0) digits.unshift(1);
+        }else{
+            return digits;
+        }
+    }
+    
+    return digits;
+};
